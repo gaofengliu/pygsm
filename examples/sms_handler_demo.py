f5a48f2c3344e69dd6082608626255ae5d87ac77
gaofengliu <gfliu@163.com>
2016-11-27 21:13:26 +0800

    handle sms with iccid

--- examples/sms_handler_demo.py
+++ examples/sms_handler_demo.py
@@ -11,7 +11,7 @@ import time
 
 from gsmmodem.modem import GsmModem
 
-PORTS =('COM3','COM4','COM6','COM7','COM8','COM9','COM10','COM11','COM12','COM13','COM14','COM15','COM16','COM17','COM18')
+PORTS =('COM5','COM6','COM7','COM8','COM9','COM10','COM11','COM12','COM13','COM14','COM15','COM17','COM18','COM19')
 modem = []
 
 BAUDRATE = 115200
@@ -28,11 +28,11 @@ sql = '''INSERT INTO `sms` (`iccid`,`OrgAddr`,`UserData`,`SmsTime`,`UpdateTime`)
         ON DUPLICATE KEY UPDATE iccid=VALUES(iccid),OrgAddr=VALUES(OrgAddr),UserData=VALUES(UserData),SmsTime=VALUES(SmsTime),UpdateTime=VALUES(UpdateTime)'''
 
 def handleSms(sms):
-    print(u'== SMS message received ==\nFrom: {0} SMSC: {1} Sim ICCID: {2}\nTime: {3}\nMessage:\n{4}\n'.format(sms.number,sms.smsc,sms.iccid, sms.time, sms.text))
+    print(u'== SMS message received ==\nFrom: {0} SMSC: {1} Sim ICCID: {2}\nTime: {3}\nMessage:\n{4}\n'.format(sms.number,sms.smsc,sms.iccid[:len(sms.iccid)-1], sms.time, sms.text))
     # Create a new record
     nowtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     with connection.cursor() as cursor:
-        cursor.execute(sql,(sms.iccid,sms.number,sms.text,sms.time,nowtime))
+        cursor.execute(sql,(sms.iccid[:len(sms.iccid)-1],sms.number,sms.text,sms.time,nowtime))
         connection.commit()
 
 def main():
@@ -59,9 +59,6 @@ def main():
     logging.getLogger('').addHandler(console)
     #################################################################################################
     _logger.debug('aaa')
-    _logger.debug('bbb')
-
-
 
 
     for i in range(len(PORTS)):
@@ -71,11 +68,11 @@ def main():
         print(u' {0} is opened\n'.format(PORTS[i]))
         print('Waiting for SMS message...')
     try:
-        for i in range(len(PORTS)):
-            modem[i].rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
+#        for i in range(len(PORTS)):
+            modem[0].rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
     finally:
-        for i in range(len(PORTS)):
-            modem[i].close();
+#        for i in range(len(PORTS)):
+            modem[0].close();
     connection.close()
 
 if __name__ == '__main__':
