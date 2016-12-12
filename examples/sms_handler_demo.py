f0e675291279da51af0c68aba4d12fdcbb6db94c
gaofengliu <gfliu@163.com>
2016-12-12 18:20:55 +0800

    modify to list ports name once programme start!

--- examples/sms_handler_demo.py
+++ examples/sms_handler_demo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """\
 Demo: handle incoming SMS messages by replying to them
 Simple demo app that listens for incoming SMS messages, displays the sender's number
@@ -6,34 +7,57 @@ and the messages, then replies to the SMS by saying "thank you"
 """
 from __future__ import print_function
 import logging
-import pymysql.cursors
+import MySQLdb.cursors
 import time
-
 from gsmmodem.modem import GsmModem
+import serial
+import serial.tools.list_ports
 
-PORTS =('COM5','COM6','COM7','COM8','COM9','COM10','COM11','COM12','COM13','COM14','COM15','COM17','COM18','COM19')
+port_list = list(serial.tools.list_ports.comports())
+
+if len(port_list) <= 0:
+    print(u'No any serial port can be find!')
+else:
+    PORTS = [port[0] for port in port_list]
+   # serlist = [serial.Serial(port_serial, 115200, timeout=60) for port_serial in port_list_coms]
+   # PORTS = [ser.name for ser in serlist]
+   # for ser in serlist:
+   #     print(u'The name of port is  > {0}\n'.format( ser.name))
+   #     ser.close()
+
 modem = []
 
 BAUDRATE = 115200
-PIN = None # SIM card PIN (if any)
+PIN = '1234' # SIM card PIN (if any)
 
-connection= pymysql.connect(host='114.55.224.65',
+connection= MySQLdb.connect('114.55.224.65',
                             user='so',
-                            password='ybkmysql123123',
+                            passwd='ybkmysql123123',
                             db='ybk',
-                            charset='utf8mb4',
-                            cursorclass=pymysql.cursors.DictCursor)
+                            charset='utf8',
+                            cursorclass=MySQLdb.cursors.DictCursor)
 
 sql = '''INSERT INTO `sms` (`iccid`,`OrgAddr`,`UserData`,`SmsTime`,`UpdateTime`) VALUES (%s, %s, %s, %s, %s)
-        ON DUPLICATE KEY UPDATE iccid=VALUES(iccid),OrgAddr=VALUES(OrgAddr),UserData=VALUES(UserData),SmsTime=VALUES(SmsTime),UpdateTime=VALUES(UpdateTime)'''
+       ON DUPLICATE KEY UPDATE iccid=VALUES(iccid),OrgAddr=VALUES(OrgAddr),UserData=VALUES(UserData),SmsTime=VALUES(SmsTime),UpdateTime=VALUES(UpdateTime)'''
 
 def handleSms(sms):
     print(u'== SMS message received ==\nFrom: {0} SMSC: {1} Sim ICCID: {2}\nTime: {3}\nMessage:\n{4}\n'.format(sms.number,sms.smsc,sms.iccid[:len(sms.iccid)-1], sms.time, sms.text))
     # Create a new record
     nowtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    with connection.cursor() as cursor:
-        cursor.execute(sql,(sms.iccid[:len(sms.iccid)-1],sms.number,sms.text,sms.time,nowtime))
+    smstime = str(sms.time)[:len(str(sms.time))-6]
+
+    try:
+        cursor=connection.cursor()
+        n=cursor.execute(sql,(sms.iccid[:len(sms.iccid)-1],
+                              sms.number,
+                              sms.text,
+                              smstime,
+                              nowtime))
         connection.commit()
+    except MySQLdb.Error, e:
+        print("Could not insert data into MySQLdb.")
+        print("Mysql Error %d: %s" % (e.args[0], e.args[1]))
+
 
 def main():
     print('Initializing modem...')
@@ -43,7 +67,7 @@ def main():
         level=logging.DEBUG,
         format='[%(asctime)s][%(process)d:%(thread)d][%(levelname)s] %(message)s',
         filename="main.log",
-        filemode='a+')
+        filemode='w')
     _logger = logging.getLogger('another.log')
     fh = logging.FileHandler('another.log')
     fh.setLevel(logging.DEBUG)
@@ -58,21 +82,36 @@ def main():
     console.setFormatter(formatter)
     logging.getLogger('').addHandler(console)
     #################################################################################################
-    _logger.debug('aaa')
+   # _logger.debug('aaa')
 
-
+#  open all modems
+    IDopened = -1
+    PORTSNOTOPEND =[]
     for i in range(len(PORTS)):
         modem.append(GsmModem(PORTS[i], BAUDRATE, smsReceivedCallbackFunc=handleSms))
         modem[i].smsTextMode = False
-        modem[i].connect(PIN)
-        print(u' {0} is opened\n'.format(PORTS[i]))
-        print('Waiting for SMS message...')
-    try:
-#        for i in range(len(PORTS)):
-            modem[0].rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
-    finally:
-#        for i in range(len(PORTS)):
-            modem[0].close();
+        try:
+            modem[i].connect(PIN)
+            IDopened +=1
+            print(u' {0} is opened\n'.format(PORTS[i]))
+
+        except:
+            PORTSNOTOPEND.append(PORTS[i])
+            print(u' {0} can not be opened\n'.format(PORTS[i]))
+            pass
+    if IDopened>-1:
+        print(u'{} Ports have not been opened!'.format(len(PORTSNOTOPEND)))
+        for p in PORTSNOTOPEND:
+            print(p)
+        try:
+            print('Waiting for SMS message...')
+            modem[IDopened].rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
+
+        finally:
+            modem[IDopened].close();
+    else:
+        print('No any Ports have been opened, check the Port !')
+
     connection.close()
 
 if __name__ == '__main__':
